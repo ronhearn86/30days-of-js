fca35977a831906c2456f1478f6f8632bf03808a
ronhearn86 <71200786+ronhearn86@users.noreply.github.com>
2022-09-04 00:19:51 -0400

    Add files via upload

---
+++ Day 1/index.js
@@ -0,0 +1,33 @@
+//comments make code readable
+//wlecome to 30 days of javascript
+/* comments can make
+code readable, and easy to reuse
+also very informative */
+
+let str = 'str'
+let bool = true
+let lame
+let und = null
+
+console.log(typeof str)
+console.log(typeof bool)
+console.log(typeof name)
+console.log(typeof und)
+
+let x
+let y
+let z
+let v
+let a = 0
+let b = 1
+let c = 2
+let d = 3
+
+const fName = 'Ron', lName = 'Hearn', mStatus = 'Married', country = "U.S.A."
+let age  = 36
+
+let myAge = 36
+let yourAge = 38
+
+console.log(`I am ${myAge} years old.
+You are ${yourAge} years old.`)
