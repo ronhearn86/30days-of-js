eb2fac42956ab568ac0c1b39a095087e6bd0cdb9
ronhearn86 <71200786+ronhearn86@users.noreply.github.com>
2022-09-04 17:45:40 -0400

    Add files via upload

---
+++ Day 3/index.js
@@ -0,0 +1,28 @@
+const now = new Date()
+let year = now.getFullYear()
+let month = now.getMonth() + 1
+let date = now.getDate()
+let day = now.getDay()
+let hour = now.getHours()
+let min = now.getMinutes()
+let allsec = Date.now()
+console.log(year)
+console.log(month)
+console.log(date)
+console.log(day + 1)
+console.log(hour)
+console.log(min)
+console.log(allsec)
+
+let x;
+
+x = prompt('enter height','lol')
+// let person = prompt("Please enter your name", "Harry Potter");
+document.getElementById('demo').addEventListener('click', prompt('Enter number', 'number goes here')) 
+//     number
+//     //let number = prompt('Enter number', 'number goes here')
+//     //let height = prompt('enter height','lol')
+//     //let x = number * height
+//     //document.write(x)
+// })
+
