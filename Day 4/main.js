2ece9046d6bb7b93ecbf7607aa35ae65ca61d624
ronhearn86 <71200786+ronhearn86@users.noreply.github.com>
2022-09-05 03:03:24 -0400

    Add files via upload

---
+++ Day 4/main.js
@@ -0,0 +1,45 @@
+let month = prompt("Enter a month").toLowerCase()
+//let b = prompt("Enter B")
+
+switch (month){
+    case ( 'january'):
+        document.write('31 Day')
+        break
+    case ( 'february'):
+        document.write('28 Day')
+        break
+    case ( 'march'):
+        document.write('31 Day')
+        break
+    case ( 'april'):
+        document.write('30 Day')
+        break
+    case ( 'may'):
+        document.write('31 Day')
+        break
+        case ( 'june'):
+            document.write('30 Day')
+            break
+            case ( 'july'):
+                document.write('31 Day')
+                break
+                case ( 'august'):
+                    document.write('31 Day')
+                    break
+                    case ( 'september'):
+                        document.write('30 Day')
+                        break
+                        case ( 'october'):
+                            document.write('31 Day')
+                            break
+                            case ( 'november'):
+                                document.write('30 Day')
+                                break
+
+ 
+    default: document.write('31 days')
+    
+    
+
+
+}
